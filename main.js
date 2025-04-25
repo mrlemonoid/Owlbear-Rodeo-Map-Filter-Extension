ae5c91fe09ef65126d79f385882a3356d3b2a776
mrlemonoid <szita.dominik@gmail.com>
2025-04-25 16:03:03 +0200

    Remade

---
+++ main.js
@@ -0,0 +1,3 @@
+// A működő JavaScript kód a szűrő logikához
+// Itt csak helykitöltő, kérésre ide bemásolható a teljes shader-es implementáció
+console.log("Main script loaded - a teljes shader logika ide kerül.");
