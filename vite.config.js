591d1fd374830010cef481486e6b216a9a8304a8
mrlemonoid <szita.dominik@gmail.com>
2025-04-26 01:13:15 +0200

    index split

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "src/index.html"
-    }
+    outDir: "dist"
   },
   optimizeDeps: {
     include: ["@owlbear-rodeo/sdk"]
